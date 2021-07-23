3903f1efb8b93d69d9813b2539f0902275292f95
Stas Bekman <stas@stason.org>
2021-07-22 17:58:00 -0700

    adjust the script

--- examples/pretrain_gpt_single_node.sh
+++ examples/pretrain_gpt_single_node.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Adapted to use deepspeed on a single node
+#
+# Multi-node will require either a `hostfile` or switching to `torch.distributed.launch`
 
 # adjust to the number of GPUs to use
-N_GPUS=2
+N_GPUS=1
 
 CHECKPOINT_PATH=checkpoints/gpt2
 VOCAB_FILE=data/gpt2-vocab.json
@@ -11,18 +13,27 @@ MERGE_FILE=data/gpt2-merges.txt
 DATA_PATH=data/meg-gpt2_text_document
 
 GPT_ARGS=" \
-    --num-layers 2 \
-    --hidden-size 64 \
-    --num-attention-heads 2 \
+    --num-layers 24 \
+    --hidden-size 1024 \
+    --num-attention-heads 16 \
     --seq-length 1024 \
     --max-position-embeddings 1024 \
     --micro-batch-size 4 \
     --global-batch-size 8 \
-    --lr 0.00015 \
-    --train-iters 50 \
+    --lr-decay-iters 320000 \
     --lr-decay-style cosine \
+    --lr 0.00015 \
+    --min-lr 1.0e-5 \
+    --lr-decay-style cosine \
+    --train-iters 5000 \
     --vocab-file $VOCAB_FILE \
     --merge-file $MERGE_FILE \
+    --data-impl mmap \
+    --split 949,50,1 \
+    --distributed-backend nccl \
+    --weight-decay 1e-2 \
+    --clip-grad 1.0 \
+    --lr-warmup-fraction .01 \
     --fp16 \
     "
 
