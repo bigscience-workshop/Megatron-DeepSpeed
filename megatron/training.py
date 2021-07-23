301f0de9ec303f8367005d3699b1922a151c5440
Stas Bekman <stas@stason.org>
2021-07-23 12:31:17 -0700

    Revert "train_samples check fix"
    This reverts commit d13f5c6bd9422986e86c67d5d91f2905a6faac52.

--- megatron/training.py
+++ megatron/training.py
@@ -835,11 +835,11 @@ def build_train_valid_test_data_iterators(
 
     # Backward compatibility, assume fixed batch size.
     if args.iteration > 0 and args.consumed_train_samples == 0:
-        assert args.train_samples is not None, \
+        assert args.train_samples is None, \
             'only backward compatiblity support for iteration-based training'
         args.consumed_train_samples = args.iteration * args.global_batch_size
     if args.iteration > 0 and args.consumed_valid_samples == 0:
-        assert args.train_samples is not None, \
+        assert args.train_samples is None, \
             'only backward compatiblity support for iteration-based training'
         args.consumed_valid_samples = (args.iteration // args.eval_interval) * \
             args.eval_iters * args.global_batch_size
