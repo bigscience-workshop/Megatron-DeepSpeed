66c32a9662ccbe1ca8f9d65b6fb6cd6c48684550
sbmaruf <sbmaruf@gmail.com>
2021-08-03 17:43:34 +0000

    export hfdataset to jsonl data

---
+++ examples/c4_mc4_processing/export_data.sh
@@ -0,0 +1,31 @@
+DATASET_NAME="mc4"
+LANG_SAMPLE_DICT_PATH="dumped/c4_mc4_raw_data_resized-576/lang_dict.json"
+CACHE_DIR="/efs/bariamzn/c4/mc4_splits/"
+OUTPUT_DIR="dumped/mc4_json_data/"
+
+mkdir -p $OUTPUT_DIR
+
+# completed am, hi, ur, bn, id, ca, zh-Latn, sw, ru-Latn, zh
+
+# for LANG in "am" "hi" "ur" "bn" "id" "ca" "zh-Latn" "sw" "ru-Latn" "zh"; do # 1
+# for LANG in "ar" ; do # 0
+# for LANG in "fr" ; do # 2
+# for LANG in "pt" ; do # 3
+# for LANG in "es" ; do # 4
+for LANG in "ru" ; do # 5
+# for LANG in "ja" ; do # 6
+    OUTPUT_LANG_DIR=$OUTPUT_DIR$LANG
+    if [ ! -d "$OUTPUT_LANG_DIR" ] 
+    then
+        mkdir -p $OUTPUT_LANG_DIR
+        python3 -u tools/c4_mc4/export_data.py \
+        --dataset-name $DATASET_NAME \
+        --language $LANG \
+        --lang-sampling-dict-path $LANG_SAMPLE_DICT_PATH \
+        --cache-dir $CACHE_DIR \
+        --output-dir $OUTPUT_LANG_DIR
+    else
+        echo "[*] Directory $OUTPUT_LANG_DIR already exists."
+        echo "[*] Aborting operation"
+    fi
+done
