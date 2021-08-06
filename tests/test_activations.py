7a68c8624520e0a2eda375f2ffc65923893229c1
Jake Tae <jaesungtae@gmail.com>
2021-08-06 22:42:15 +0900

    test: use seed-controlled random batch inputs

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,43 +1,43 @@
-import math
+import random
 import unittest
 
 import torch
+from torch.nn import functional as F
 
 from megatron.model.activations import liglu, geglu, reglu, swiglu
+
+from .utils import set_seed
 
 
 class TestActivations(unittest.TestCase):
     def setUp(self):
-        self.x = torch.tensor([0.1, -0.7])
+        """setup an input of reasonable size"""
+        set_seed()
+        self.batch_size = random.randint(2, 64)
+        self.seq_len = random.randint(256, 1025)
+        self.num_channels = random.randint(1, 384) * 2
+        self.x = torch.randn(self.batch_size, self.seq_len, self.num_channels)
+        self.x1, self.x2 = self.x.chunk(2, dim=-1)
 
     def test_shapes(self):
-        batch_size = 8
-        seq_len = 100
-        num_channels = 768
-        x = torch.randn(batch_size, seq_len, num_channels)
         # glu should halve the last dimension
-        output_shape = [batch_size, seq_len, num_channels // 2]
+        output_shape = [self.batch_size, self.seq_len, self.num_channels // 2]
         for activation_fn in [liglu, geglu, reglu, swiglu]:
-            output = activation_fn(x)
+            output = activation_fn(self.x)
             self.assertEqual(list(output.shape), output_shape)
 
     def test_liglu(self):
-        expected = torch.tensor([-0.07])
+        expected = self.x1 * self.x2
         torch.testing.assert_equal(liglu(self.x), expected)
-    
+ 
     def test_geglu(self):
-        """compute gelu output according to its definition"""
-        normal_cdf = lambda x: 0.5 * (1 + math.erf(x / math.sqrt(2)))
-        gelu_output = self.x[1] * normal_cdf(self.x[1].item())
-        expected = self.x[0] * gelu_output
-        self.assertAlmostEqual(geglu(self.x).item(), expected.item())
+        expected = self.x1 * F.gelu(self.x2)
+        torch.testing.assert_equal(geglu(self.x), expected)
         
     def test_reglu(self):
-        expected = torch.tensor([0.])
+        expected = self.x1 * F.relu(self.x2)
         torch.testing.assert_equal(reglu(self.x), expected)
 
     def test_swiglu(self):
-        """compute swish output according to its definition"""
-        swish_output = self.x[1] / (1 + math.pow(math.e, -self.x[1].item()))
-        expected = self.x[0] * swish_output
-        self.assertAlmostEqual(swiglu(self.x).item(), expected.item())
+        expected = self.x1 * F.silu(self.x2)
+        torch.testing.assert_equal(swiglu(self.x), expected)
