47244d0d792c20a66f29c5bddbe60ecde60dbb32
M Saiful Bari <sbmaruf@gmail.com>
2021-08-07 10:30:15 +0000

    cleaning, moving scripts

--- scripts/c4_mc4_processing/data_process.sh
+++ scripts/c4_mc4_processing/data_process.sh
@@ -1,19 +1,44 @@
-for LANG in "am" "hi" "ur" "bn" "id" "ca" "zh-Latn" "sw" "ru-Latn" "zh"; do
-# for LANG in "ar" "ja"; do
-# for LANG in "pt"; do
+
+# Process single shard data
+# for lang in "am" "hi" "ur" "bn" "id" "ca" "zh-Latn" "sw" "ru-Latn" "zh" "ar" "ja" "pt" "es" "fr"; do
+
+for lang in "am"; do
     python tools/preprocess_data.py \
-        --input dumped/mc4_json_data/$LANG/train.jsonl \
-        --output-prefix dumped/mc4_json_data/$LANG/train \
+        --input dumped/mc4_json_data/$lang/train.jsonl \
+        --output-prefix dumped/mc4_json_data/$lang/train \
         --dataset-impl mmap \
         --tokenizer-type PretrainedFromHF \
-        --tokenizer-name-or-path "google/mt5-large" \
+        --tokenizer-name-or-path "google/mt5-base" \
         --workers 92
     
     python tools/preprocess_data.py \
-        --input dumped/mc4_json_data/$LANG/validation.jsonl \
-        --output-prefix dumped/mc4_json_data/$LANG/validation \
+        --input dumped/mc4_json_data/$lang/validation.jsonl \
+        --output-prefix dumped/mc4_json_data/$lang/validation \
         --dataset-impl mmap \
         --tokenizer-type PretrainedFromHF \
-        --tokenizer-name-or-path "google/mt5-large" \
+        --tokenizer-name-or-path "google/mt5-base" \
         --workers 92
 done
+
+
+
+# Process multiple shard data
+lang='en'
+cnt=0
+for _file in dumped/mc4_json_data/$lang/validation* ; do
+    PREFIX=${_file%.*}
+    python tools/preprocess_data.py \
+        --input $_file \
+        --output-prefix $PREFIX \
+        --dataset-impl mmap \
+        --tokenizer-type PretrainedFromHF \
+        --tokenizer-name-or-path "google/mt5-base" \
+        --workers 92
+done
+
+
+python3 tools/merge_preprocessed_data.py \
+  --datasets dumped/mc4_json_data/en/validation.*.bin \
+  --output-prefix dumped/mc4_json_data/en/validation
+
+
