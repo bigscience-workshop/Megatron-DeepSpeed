cb5e674b2af24bdec2f9a290a100b0b95a8c37f5
Adam Moody <moody20@llnl.gov>
2021-08-15 16:21:02 -0700

    update merge_preprocessed_data to use parallel merge

--- tools/merge_preprocessed_data.py
+++ tools/merge_preprocessed_data.py
@@ -1,8 +1,15 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             os.path.pardir)))
+
 import argparse
 import time
 
+from megatron import print_rank_0
 from megatron.data import indexed_dataset
-from megatron.data.indexed_dataset import infer_dataset_impl, MMapIndexedDataset, data_file_path, index_file_path
+from megatron.data.indexed_dataset import infer_dataset_impl, MMapIndexedDataset, data_file_path, index_file_path, merge_files_dist
+from megatron.data.distdata import DistData
 
 
 def get_args():
@@ -15,7 +22,21 @@ def get_args():
     group.add_argument('--output-prefix', type=str, required=True,
                        help='Path to binary output file without suffix')
 
-    return parser.parse_args()
+    group = parser.add_argument_group(title='runtime')
+    group.add_argument('--merge', type=str, default='parallel', choices = ['parallel', 'serial'],
+                       help='Whether to use a distributed parallal merge or a non-distributed serial merge.')
+    group.add_argument('--torch-backend', type=str, default='gloo', choices = ['gloo', 'mpi'],
+                       help='Select torch.distributed backend.')
+    group.add_argument('--local_rank', type=int, default=None,
+                       help='Local rank of calling process on its node (from torch.distributed.launch).')
+
+    args = parser.parse_args()
+
+    # initialize distributed environment if parallel merge requested
+    if args.merge == 'parallel':
+        args.distctx = DistData(backend=args.torch_backend)
+
+    return args
 
 def main():
     """
@@ -24,33 +45,35 @@ def main():
     args = get_args()
     startup_start = time.time()
 
-    print("Merging", args.datasets)
+    print_rank_0(f"Merging {args.datasets}")
+    print_rank_0(f"Output prefix: {args.output_prefix}")
 
-    # We use the first dataset to infer the dataset implementation common to all datasets.
-    dataset_impl = infer_dataset_impl(args.datasets[0])
-    assert dataset_impl is not None
+    if args.merge == 'parallel':
+        merge_files_dist(args.output_prefix, args.datasets, args.distctx)
+    else:
+        # We use the first dataset to infer the dataset implementation common to all datasets.
+        dataset_impl = infer_dataset_impl(args.datasets[0])
+        assert dataset_impl is not None
 
-    first_dataset = indexed_dataset.make_dataset(args.datasets[0], dataset_impl)
-    # We use the first dataset to infer the dtype common to all datasets.
-    dtype = first_dataset.dtype if isinstance(first_dataset, MMapIndexedDataset) else None
+        first_dataset = indexed_dataset.make_dataset(args.datasets[0], dataset_impl)
+        # We use the first dataset to infer the dtype common to all datasets.
+        dtype = first_dataset.dtype if isinstance(first_dataset, MMapIndexedDataset) else None
 
-    print(f"Output prefix: {args.output_prefix}")
+        output_filename = args.output_prefix
+        output_bin_file = data_file_path(output_filename)
+        output_idx_file = index_file_path(output_filename)
+        builder = indexed_dataset.make_builder(output_bin_file,
+                                               impl=dataset_impl,
+                                               dtype=dtype)
+        for dataset in args.datasets:
+            builder.merge_file_(dataset)
 
-    output_filename = args.output_prefix
-    output_bin_file = data_file_path(output_filename)
-    output_idx_file = index_file_path(output_filename)
-    builder = indexed_dataset.make_builder(output_bin_file,
-                                           impl=dataset_impl,
-                                           dtype=dtype)
-    for dataset in args.datasets:
-        builder.merge_file_(dataset)
-
-    builder.finalize(output_idx_file)
+        builder.finalize(output_idx_file)
 
     startup_end = time.time()
-    print("Time to merge:", startup_end - startup_start)
+    print_rank_0(f"Time to merge: {startup_end - startup_start")
 
-    print(f"Merged {len(args.datasets)} datasets to {args.output_prefix}")
+    print_rank_0(f"Merged {len(args.datasets)} datasets to {args.output_prefix}")
 
 if __name__ == "__main__":
     main()
