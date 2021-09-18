f320d45a7a4d70b7e9c0000cf5850425a6dda564
Stas Bekman <stas@stason.org>
2021-09-17 17:17:04 -0700

    [requirements] check we test agains the correct deepspeed branch

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,8 +17,8 @@
 
 import sys
 import warnings
+import deepspeed
 from os.path import abspath, dirname, join
-
 
 # allow having multiple repository checkouts and not needing to remember to rerun
 # 'pip install -e .[dev]' when switching between checkouts and running tests.
@@ -28,6 +28,14 @@ sys.path.insert(1, git_repo_path)
 # silence FutureWarning warnings in tests since often we can't act on them until
 # they become normal warnings - i.e. the tests still need to test the current functionality
 warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+def pytest_sessionstart(session):
+    # since some of us use use deepspeed@master for other uses make sure we test against the correct
+    # deepspeed branch. eventually this won't be needed by for now we use a frozen branch
+    if deepspeed.__git_branch__ != "big-science":
+        raise ValueError(f"detected deepspeed.__git_branch__={deepspeed.__git_branch__ }, but needing branch='big-science'.\n"
+                         "You can install it with: pip install git+https://github.com/microsoft/DeepSpeed.git@big-science")
 
 
 def pytest_sessionfinish(session, exitstatus):
