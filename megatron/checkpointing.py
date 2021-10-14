5467a59e1b5882e29a3fd3afe6c90c43e3a51727
SaulLu <lucilesaul.com@gmail.com>
2021-10-14 11:51:35 +0200

    don't do `compare('max_position_embeddings')` for alibi

--- megatron/checkpointing.py
+++ megatron/checkpointing.py
@@ -28,6 +28,7 @@ from megatron import (get_args,
                       print_rank_0,
                       update_num_microbatches,
                       utils)
+from megatron.enums import PositionEmbeddingType
 
 _CHECKPOINT_VERSION = None
 
@@ -61,8 +62,10 @@ def check_checkpoint_args(checkpoint_args):
     _compare('num_layers')
     _compare('hidden_size')
     _compare('num_attention_heads')
-    _compare('max_position_embeddings')
     _compare('position_embedding_type')
+    # with alibi we can change `max_position_embeddings`
+    if args.position_embedding_type != PositionEmbeddingType.alibi:
+        _compare('max_position_embeddings')
     if args.vocab_file:
         _compare('make_vocab_size_divisible_by')
         _compare('padded_vocab_size')
