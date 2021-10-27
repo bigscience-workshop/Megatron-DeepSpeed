eabb8c5adbd961a2be39612e6e629156d4264c53
Jake Tae <>
2021-10-27 02:24:32 -0400

    fix: use named_modules, add min max logging

--- megatron/debug_utils.py
+++ megatron/debug_utils.py
@@ -7,15 +7,18 @@ class DebugGradientNorm:
             except TypeError:
                 self.module2key = {getattr(model, layer): layer for layer in layers}
         else:
-            self.module2key = {module: name for name, module in model.named_children()}
+            self.module2key = {module: name for name, module in model.named_parameters()}
 
     def backward_hook(self, module, grad_inputs, grad_outputs):
         total_norm = sum(grad_output.norm() ** 2 for grad_output in grad_outputs)
-        print(self.module2key[module], total_norm.sqrt().item())
+        total_norm_squared = total_norm.sqrt().item()
+        min_grad_norm = grad_inputs.min().item()
+        max_grad_norm = grad_inputs.max().item()
+        module_name = self.module2key[module]
+        print(f"{module_name}: total {total_norm_squared}, min {min_grad_norm}, max {max_grad_norm}")
 
     def register_backward_hook(self):
-        for module in self.module2key:
-            module.apply(self._register_backward_hook)
+        self.model.apply(self._register_backward_hook)
 
     def _register_backward_hook(self, module):
         if hasattr(module, "register_full_backward_hook"):
