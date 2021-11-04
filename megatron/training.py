cf5cb451aaa44ca19f355fe8d0d8242f9c69fee1
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2021-11-04 11:38:41 +0100

    Woops

--- megatron/training.py
+++ megatron/training.py
@@ -1027,7 +1027,7 @@ def build_train_valid_test_data_iterators(
                             if test_ds is not None else []
 
         # Flags to know if we need to do training/validation/testing.
-        do_train = len(train_dataloader) > 0 and args.train_iters > 0
+        do_train = train_dataloader is not None and args.train_iters > 0
 
         # Need to broadcast num_tokens and num_type_tokens.
         flags = torch.cuda.LongTensor([
