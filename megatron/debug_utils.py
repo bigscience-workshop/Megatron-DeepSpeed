5edefea439ba5d16581e2be60bbe339b8f3914a1
Stas Bekman <stas@stason.org>
2021-11-17 21:44:54 -0800

    reworking the state dumper

--- megatron/debug_utils.py
+++ megatron/debug_utils.py
@@ -4,151 +4,34 @@ import collections
 
 from packaging import version
 
+from torch.utils.tensorboard import SummaryWriter
+
 class DebugUnderflowOverflow:
     """
-    This debug class helps detect and understand where the model starts getting very large or very small, and more
-    importantly ``nan`` or ``inf`` weight and activation elements.
-
-    There are 2 working modes:
-
-    1. Underflow/overflow detection (default)
-    2. Specific batch absolute min/max tracing without detection
-
-    Mode 1: Underflow/overflow detection
-
-    To activate the underflow/overflow detection, initialize the object with the model ::
-
-        debug_overflow = DebugUnderflowOverflow(model)
-
-    then run the training as normal and if ``nan`` or ``inf`` gets detected in at least one of the weight, input or
-    output elements this module will throw an exception and will print ``max_frames_to_save`` frames that lead to this
-    event, each frame reporting
-
-    1. the fully qualified module name plus the class name whose ``forward`` was run
-    2. the absolute min and max value of all elements for each module weights, and the inputs and output
-
-    For example, here is the header and the last few frames in detection report for ``google/mt5-small`` run in fp16 mixed precision ::
-
-        Detected inf/nan during batch_number=0
-        Last 21 forward frames:
-        abs min  abs max  metadata
-        [...]
-                          encoder.block.2.layer.1.DenseReluDense.wi_0 Linear
-        2.17e-07 4.50e+00 weight
-        1.79e-06 4.65e+00 input[0]
-        2.68e-06 3.70e+01 output
-                          encoder.block.2.layer.1.DenseReluDense.wi_1 Linear
-        8.08e-07 2.66e+01 weight
-        1.79e-06 4.65e+00 input[0]
-        1.27e-04 2.37e+02 output
-                          encoder.block.2.layer.1.DenseReluDense.wo Linear
-        1.01e-06 6.44e+00 weight
-        0.00e+00 9.74e+03 input[0]
-        3.18e-04 6.27e+04 output
-                          encoder.block.2.layer.1.DenseReluDense T5DenseGatedGeluDense
-        1.79e-06 4.65e+00 input[0]
-        3.18e-04 6.27e+04 output
-                          encoder.block.2.layer.1.dropout Dropout
-        3.18e-04 6.27e+04 input[0]
-        0.00e+00      inf output
-
-    You can see here, that ``T5DenseGatedGeluDense.forward`` resulted in output activations, whose absolute max value
-    was around 62.7K, which is very close to fp16's top limit of 64K. In the next frame we have ``Dropout`` which
-    renormalizes the weights, after it zeroed some of the elements, which pushes the absolute max value to more than
-    64K, and we get an overlow.
-
-    As you can see it's the previous frames that we need to look into when the numbers start going into very large for
-    fp16 numbers.
-
-    The tracking is done in a forward hook, which gets invoked immediately after ``forward`` has completed.
-
-    By default the last 21 frames are printed. You can change the default to adjust for your needs. For example ::
-
-        debug_overflow = DebugUnderflowOverflow(model, max_frames_to_save=100)
-
-    To validate that you have set up this debugging feature correctly, and you intend to use it in a training that may
-    take hours to complete, first run it with normal tracing enabled for one of a few batches as explained in the next
-    section.
-
-
-    Mode 2. Specific batch absolute min/max tracing without detection
-
-    The second work mode is per-batch tracing with the underflow/overflow detection feature turned off.
-
-    Let's say you want to watch the absolute min and max values for all the ingredients of each ``forward`` call of a
-    given batch, and only do that for batches 1 and 3. Then you instantiate this class as ::
-
-        debug_overflow = DebugUnderflowOverflow(model, trace_batch_nums=[1,3])
-
-    And now full batches 1 and 3 will be traced using the same format as explained above. Batches are 0-indexed.
-
-    This is helpful if you know that the program starts misbehaving after a certain batch number, so you can
-    fast-forward right to that area.
-
-
-    Early stopping:
-
-    You can also specify the batch number after which to stop the training, with ::
-
-        debug_overflow = DebugUnderflowOverflow(model, trace_batch_nums=[1,3], abort_after_batch_num=3)
-
-    This feature is mainly useful in the tracing mode, but you can use it for any mode.
-
-
-    **Performance**:
-
-    As this module measures absolute ``min``/``max`` of each weight of the model on every forward it'll slow the
-    training down. Therefore remember to turn it off once the debugging needs have been met.
 
     Args:
+        args (:obj:`dict`):
+            Megatron args object
         model (:obj:`nn.Module`):
             The model to debug.
-        max_frames_to_save (:obj:`int`, `optional`, defaults to 21):
-            How many frames back to record
-        trace_batch_nums(:obj:`List[int]`, `optional`, defaults to ``[]``):
-            Which batch numbers to trace (turns detection off)
-        abort_after_batch_num  (:obj:`int`, `optional`):
-            Whether to abort after a certain batch number has finished
-        print_fh (:obj:`_io.TextIOWrapper`, `optional`, defaults to ``sys.stdout``):
-            Filehandle to print to.
-
     """
 
-    def __init__(self, args, model, print_fh=sys.stdout):
+    def __init__(self, args, model):
         self.args = args
         self.model = model
-        self.print_fh = print_fh
         self.tb_path = args.tensorboard_debug_dir
         self.rank = args.rank
-
         self.tbs = {}
 
-        self.frame = []
-        self.prefix = "                 "
+        # XXX: possible additions
+        # - iteration-interval: don't log on each iteration to have less data
+        # - log-on-threshold: log only when values are larger than certain value - e.g. 75% of 64k (max fp16 number before overflow)
+        # - the caller could also skip logging on all layers/pipe stages
 
         self.analyse_model()
 
         self.register_forward_hook()
-
-    def print(self, *args, **kwargs):
-        print(*args, **kwargs, file=self.print_fh)
-
-    def save_frame(self, frame=None):
-        if frame is not None:
-            self.expand_frame(frame)
-        self.frames.append("\n".join(self.frame))
-        self.frame = []  # start a new frame
-
-    def expand_frame(self, line):
-        self.frame.append(line)
-
-    def trace_frames(self):
-        self.print("\n".join(self.frames))
-        self.frames = []
-
-    def reset_saved_frames(self):
-        self.frames = []
-
+        self.register_backward_hook()
 
     def analyse_model(self):
         # extract the fully qualified module names, to be able to report at run time. e.g.:
@@ -156,27 +39,66 @@ class DebugUnderflowOverflow:
         #
         # for shared weights only the first shared module name will be registered
         self.module_names = {m: name for name, m in self.model.named_modules()}
-        # self.longest_module_name = max(len(v) for v in self.module_names.values())
 
-    def analyse_variable(self, var, ctx):
-        if torch.is_tensor(var):
-            self.expand_frame(get_abs_min_max(var, ctx))
+    def register_forward_hook(self):
+        self.model.apply(self._register_forward_hook)
 
-    def create_frame(self, module, input, output):
+    def register_backward_hook(self):
+        self.model.apply(self._register_backward_hook)
 
-        # XXX: hack
-        self.frame = []
+    def _register_forward_hook(self, module):
+        module.register_forward_hook(self.forward_hook)
+
+    def _register_backward_hook(self, module):
+        # XXX: register_full_backward_hook leads to a huge leak
+        # https://github.com/microsoft/DeepSpeed/issues/1572 so for now will use the deprecated
+        # function:
+        module.register_backward_hook(self.backward_hook)
+        # if version.parse(torch.__version__) >= version.parse("1.10"):
+        #     module.register_full_backward_hook(self.backward_hook)
+        # else:
+        #     module.register_backward_hook(self.backward_hook)
+
+    def forward_hook(self, module, input, output):
+        # - input is a tuple of packed inputs (could be non-Tensors)
+        # - output could be a Tensor or a tuple of Tensors and non-Tensors
+        self.process(module, input, output, direction='forward')
+
+    def backward_hook(self, module, input, output):
+        self.process(module, input, output, direction='backward')
+
+    def process(self, module, input, output, direction):
+        iteration = self.args.iteration
+
+        tb_dir = f"{self.tb_path}/{self.rank:0>3d}-{self.module_names[module]}"
+
+        if tb_dir not in self.tbs:
+            print(f"Allocating new writer for {tb_dir}")
+            self.tbs[tb_dir] = SummaryWriter(log_dir=tb_dir, max_queue=5)
+
+        tb = self.tbs[tb_dir]
+
+        def log_to_tb(var, name):
+            if not (torch.is_tensor(var) and var.is_floating_point() and var is not None):
+                return
+            # need to prefix with numbers to get TB to sort graphs the way we want
+            prefix = "0.fwd" if direction == 'forward' else "1.bwd"
+            prefix += f"/{name}"
+            abs_var = var.abs()
+            tb.add_scalar(f"{prefix}/0.abs_min", abs_var.min(), iteration)
+            tb.add_scalar(f"{prefix}/1.abs_max", abs_var.max(), iteration)
+            tb.add_scalar(f"{prefix}/2.norm", torch.linalg.norm(var.data), iteration)
 
         # params
         for name, p in module.named_parameters(recurse=False):
-            self.analyse_variable(p, name)
+            log_to_tb(p, name)
 
         # inputs
         if isinstance(input, tuple):
             for i, x in enumerate(input):
-                self.analyse_variable(x, f"input[{i}]")
+                log_to_tb(x, f"input[{i}]")
         else:
-            self.analyse_variable(input, "input")
+            log_to_tb(input, "input")
 
         # outputs
         if isinstance(output, tuple):
@@ -184,162 +106,11 @@ class DebugUnderflowOverflow:
                 # possibly a tuple of tuples
                 if isinstance(x, tuple):
                     for j, y in enumerate(x):
-                        self.analyse_variable(y, f"output[{i}][{j}]")
+                        log_to_tb(y, f"output[{i}][{j}]")
                 else:
-                    self.analyse_variable(x, f"output[{i}]")
+                    log_to_tb(x, f"output[{i}]")
         else:
-            self.analyse_variable(output, "output")
-
-
-        # # XXX: configurable path prefix
-        # file = f"debug-logs/max/{self.rank:0>3d}-{self.module_names[module]}.csv"
-        # first_time = False
-        # if file not in self.file_handles:
-        #     first_time = True
-        #     # self.file_handles[file] = open(file, "a")
-        #     log_dir = f"debug-logs/tbs/{self.rank:0>3d}-{self.module_names[module]}"
-        # fh = self.file_handles[file]
-
-        tb_dir = f"{self.tb_path}/{self.rank:0>3d}-{self.module_names[module]}"
-        first_time = False
-        if tb_dir not in self.tbs:
-            first_time = True
-            from torch.utils.tensorboard import SummaryWriter
-            self.tbs[tb_dir] = SummaryWriter(log_dir=tb_dir, max_queue=5)
-
-        tb = self.tbs[tb_dir]
-
-        # XXX: refactor
-        frame_header, *records = self.frame
-
-        # print(self.frame)
-        # print(frame_header)
-        #print(records)
-        # die
-
-        # headers = ["batch_number"]
-        # # XXX: just max for now
-        # records_max = [str(self.batch_number)]
-        for l in records:
-            if "not a tensor" in l:
-                continue
-            segments = l.split()
-            if len(segments) != 3:
-                continue
-            rec_min, rec_max, name = segments
-            # if first_time:
-            #     headers.append(name)
-            tb.add_scalar(name, float(rec_max), self.args.iteration)
-
-        #     records_max.append(str(rec_max))
-        # if first_time:
-        #     print(",".join(headers), file=fh)
-        # print(",".join(records_max), file=fh)
-
-        # self.save_frame()
-
-    def register_forward_hook(self):
-        self.model.apply(self._register_forward_hook)
-
-    def _register_forward_hook(self, module):
-        module.register_forward_hook(self.forward_hook)
-
-    def forward_hook(self, module, input, output):
-        # - input is a tuple of packed inputs (could be non-Tensors)
-        # - output could be a Tensor or a tuple of Tensors and non-Tensors
-
-        last_frame_of_batch = False
-
-        # trace_mode = True if self.batch_number in self.trace_batch_nums else False
-        # if trace_mode:
-        #     self.reset_saved_frames()
-
-        # if self.total_calls == 0:
-        #     self.batch_start_frame()
-        # self.total_calls += 1
-
-        # count batch numbers - the very first forward hook of the batch will be called when the
-        # batch completes - i.e. it gets called very last - we know this batch has finished
-        if module == self.model:
-            self.batch_number += 1
-            last_frame_of_batch = True
-
-        self.create_frame(module, input, output)
-
-        # if last_frame_of_batch:
-        #     self.batch_end_frame()
-
-        # if trace_mode:
-        #     self.trace_frames()
-
-        # if last_frame_of_batch:
-        #     self.batch_start_frame()
-
-        # if self.detected_overflow and not trace_mode:
-        #     self.dump_saved_frames()
-
-        #     # now we can abort, as it's pointless to continue running
-        #     raise ValueError(
-        #         "DebugUnderflowOverflow: inf/nan detected, aborting as there is no point running further. "
-        #         "Please scroll up above this traceback to see the activation values prior to this event."
-        #     )
-
-        # # abort after certain batch if requested to do so
-        # if self.abort_after_batch_num is not None and self.batch_number > self.abort_after_batch_num:
-        #     raise ValueError(
-        #         f"DebugUnderflowOverflow: aborting after {self.batch_number} batches due to `abort_after_batch_num={self.abort_after_batch_num}` arg"
-        #     )
-
-
-def get_abs_min_max(var, ctx):
-    abs_var = var.abs()
-    return f"{abs_var.min():8.2e} {abs_var.max():8.2e} {ctx}"
-
-
-def detect_overflow(var, ctx):
-    """
-    Report whether the tensor contains any ``nan`` or ``inf`` entries.
-
-    This is useful for detecting overflows/underflows and best to call right after the function that did some math that
-    modified the tensor in question.
-
-    This function contains a few other helper features that you can enable and tweak directly if you want to track
-    various other things.
-
-    Args:
-        var: the tensor variable to check
-        ctx: the message to print as a context
-
-    Return:
-        :obj:`True` if ``inf`` or ``nan`` was detected, :obj:`False` otherwise
-    """
-    detected = False
-    if torch.isnan(var).any().item():
-        detected = True
-        print(f"{ctx} has nans")
-    if torch.isinf(var).any().item():
-        detected = True
-        print(f"{ctx} has infs")
-
-    # if needed to monitor large elements can enable the following
-    if 0:  # and detected:
-        n100 = var[torch.ge(var.abs(), 100)]
-        if n100.numel() > 0:
-            print(f"{ctx}:  n100={n100.numel()}")
-        n1000 = var[torch.ge(var.abs(), 1000)]
-        if n1000.numel() > 0:
-            print(f"{ctx}: n1000={n1000.numel()}")
-        n10000 = var[torch.ge(var.abs(), 10000)]
-        if n10000.numel() > 0:
-            print(f"{ctx}: n10000={n10000.numel()}")
-
-    if 0:
-        print(f"min={var.min():9.2e} max={var.max():9.2e}")
-
-    if 0:
-        print(f"min={var.min():9.2e} max={var.max():9.2e} var={var.var():9.2e} mean={var.mean():9.2e} ({ctx})")
-
-    return detected
+            log_to_tb(output, "output")
 
 
 class DebugGradientNorm:
