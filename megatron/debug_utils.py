8327d44a50c652ea76eb61fb6352d45ce6ba416d
Stas Bekman <stas@stason.org>
2021-11-22 20:03:05 -0800

    backport DebugUnderflowOverflow

---
+++ megatron/debug_utils.py
@@ -0,0 +1,114 @@
+import torch
+import sys
+import collections
+
+from packaging import version
+
+from torch.utils.tensorboard import SummaryWriter
+
+class DebugUnderflowOverflow:
+    """
+
+    Args:
+        args (:obj:`dict`):
+            Megatron args object
+        model (:obj:`nn.Module`):
+            The model to debug.
+    """
+
+    def __init__(self, args, model):
+        self.args = args
+        self.model = model
+        self.tb_path = args.tensorboard_debug_dir
+        self.rank = args.rank
+        self.tbs = {}
+
+        # XXX: possible additions
+        # - iteration-interval: don't log on each iteration to have less data
+        # - log-on-threshold: log only when values are larger than certain value - e.g. 75% of 64k (max fp16 number before overflow)
+        # - the caller could also skip logging on all layers/pipe stages
+
+        self.analyse_model()
+
+        self.register_forward_hook()
+        self.register_backward_hook()
+
+    def analyse_model(self):
+        # extract the fully qualified module names, to be able to report at run time. e.g.:
+        # encoder.block.2.layer.0.SelfAttention.o
+        #
+        # for shared weights only the first shared module name will be registered
+        self.module_names = {m: name for name, m in self.model.named_modules()}
+
+    def register_forward_hook(self):
+        self.model.apply(self._register_forward_hook)
+
+    def register_backward_hook(self):
+        self.model.apply(self._register_backward_hook)
+
+    def _register_forward_hook(self, module):
+        module.register_forward_hook(self.forward_hook)
+
+    def _register_backward_hook(self, module):
+        # XXX: register_full_backward_hook leads to a huge leak
+        # https://github.com/microsoft/DeepSpeed/issues/1572 so for now will use the deprecated
+        # function:
+        module.register_backward_hook(self.backward_hook)
+        # if version.parse(torch.__version__) >= version.parse("1.10"):
+        #     module.register_full_backward_hook(self.backward_hook)
+        # else:
+        #     module.register_backward_hook(self.backward_hook)
+
+    def forward_hook(self, module, input, output):
+        # - input is a tuple of packed inputs (could be non-Tensors)
+        # - output could be a Tensor or a tuple of Tensors and non-Tensors
+        self.process(module, input, output, direction='forward')
+
+    def backward_hook(self, module, input, output):
+        self.process(module, input, output, direction='backward')
+
+    def process(self, module, input, output, direction):
+        iteration = self.args.iteration
+
+        tb_dir = f"{self.tb_path}/{self.rank:0>3d}-{self.module_names[module]}"
+
+        if tb_dir not in self.tbs:
+            print(f"Allocating new writer for {tb_dir}")
+            self.tbs[tb_dir] = SummaryWriter(log_dir=tb_dir, max_queue=5)
+
+        tb = self.tbs[tb_dir]
+
+        def log_to_tb(var, name):
+            if not (torch.is_tensor(var) and var.is_floating_point() and var is not None):
+                return
+            # need to prefix with numbers to get TB to sort graphs the way we want
+            prefix = "0.fwd" if direction == 'forward' else "1.bwd"
+            prefix += f"/{name}"
+            abs_var = var.abs()
+            tb.add_scalar(f"{prefix}/0.abs_min", abs_var.min(), iteration)
+            tb.add_scalar(f"{prefix}/1.abs_max", abs_var.max(), iteration)
+            tb.add_scalar(f"{prefix}/2.norm", torch.linalg.norm(var.data), iteration)
+
+        # XXX: can probably skip this one for direction == 'backward' as the same is already logged for fwd
+        # params
+        for name, p in module.named_parameters(recurse=False):
+            log_to_tb(p, name)
+
+        # inputs
+        if isinstance(input, tuple):
+            for i, x in enumerate(input):
+                log_to_tb(x, f"input[{i}]")
+        else:
+            log_to_tb(input, "input")
+
+        # outputs
+        if isinstance(output, tuple):
+            for i, x in enumerate(output):
+                # possibly a tuple of tuples
+                if isinstance(x, tuple):
+                    for j, y in enumerate(x):
+                        log_to_tb(y, f"output[{i}][{j}]")
+                else:
+                    log_to_tb(x, f"output[{i}]")
+        else:
+            log_to_tb(output, "output")
