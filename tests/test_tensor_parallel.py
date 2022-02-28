5c6dec09bd9a616bef1100cd732d5fc703e08505
SaulLu <lucilesaul.com@gmail.com>
2022-02-28 18:02:52 +0100

    pad to save vocab size

--- tests/test_tensor_parallel.py
+++ tests/test_tensor_parallel.py
@@ -170,6 +170,7 @@ class MegDSTestTP(TestCasePlus):
         cp_dir = self.get_auto_remove_tmp_dir()
         
         command_args = self.get_default_args()
+        command_args["--pad-vocab-size-to"] = "50432"
         command_args["--position-embedding-type"] = "alibi"
         command_args["--tensor-model-parallel-size"] = "1"
         
