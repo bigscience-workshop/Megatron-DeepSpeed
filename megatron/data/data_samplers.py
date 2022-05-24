10f50184c69fb9d1dc9c72077064846179c71f97
Stas Bekman <stas@stason.org>
2022-05-24 17:15:22 +0200

    revert generator sync

--- megatron/data/data_samplers.py
+++ megatron/data/data_samplers.py
@@ -55,6 +55,7 @@ def build_pretraining_data_loader(dataset, consumed_samples, num_workers=None):
     return torch.utils.data.DataLoader(dataset,
                                        batch_sampler=batch_sampler,
                                        num_workers=num_workers,
+                                       generator=torch.Generator().manual_seed(args.seed),
                                        pin_memory=True)
 
 class MegatronPretrainingSampler:
