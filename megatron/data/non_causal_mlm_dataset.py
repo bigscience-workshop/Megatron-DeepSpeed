62d3e3e9cbdd3817f4d91b4044134ab1714f3339
Lintang Sutawika <lintang@datasaur.ai>
2022-06-02 20:31:09 +0700

    minor fix

--- megatron/data/non_causal_mlm_dataset.py
+++ megatron/data/non_causal_mlm_dataset.py
@@ -291,6 +291,13 @@ def build_training_sample(sample, target_seq_length,
     input_tokens_ids = filter_input_ids(tokens, input_ids_sentinel, eos_id)[0]
     output_tokens_ids = filter_input_ids(tokens, labels_sentinel, eos_id)[0]
 
+    print("input_tokens_ids")
+    print(len(input_tokens_ids))
+    print(input_tokens_ids)
+    print("output_tokens_ids")
+    print(len(output_tokens_ids))
+    print(output_tokens_ids)
+
     # # Masking.
     # max_predictions_per_seq = masked_lm_prob * max_num_tokens
     # (tokens, masked_positions, masked_labels, _, masked_spans) = create_masked_lm_predictions(
