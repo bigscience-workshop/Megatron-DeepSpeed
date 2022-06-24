9daa3766d21546397d7a3cf257f86ac21cf343ba
Lintang Sutawika <lintang@datasaur.ai>
2022-06-24 13:16:37 +0700

    removed unused files

--- tests/test_packing_dataloader.py
+++ tests/test_packing_dataloader.py
@@ -6,6 +6,26 @@ from megatron.initialize import initialize_megatron
 # from megatron.data.gpt_dataset import build_train_valid_test_datasets
 from megatron.data.non_causal_mtf_dataset import build_train_valid_test_datasets
 
+from datasets import load_dataset
+
+import torch.distributed as dist
+
+# To preprocess data before testing
+# python tools/preprocess_data.py \
+#     --input tests/data/t0/ag_news_classify_question_first.json \
+#     --output-prefix tests/data/t0/ag_news_prompt \
+#     --dataset-impl mmap \
+#     --tokenizer-type PretrainedFromHF \
+#     --tokenizer-name-or-path gpt2 \
+#     --append-eod \
+#     --workers 8
+
+os.environ['MASTER_ADDR'] = 'localhost'
+os.environ['MASTER_PORT'] = '12355'
+
+# initialize the process group
+dist.init_process_group("nccl", rank=0, world_size=1)
+
 #Initialize Megatron with dummy variables
 initialize_megatron(
     extra_args_provider=None,
@@ -16,6 +36,9 @@ initialize_megatron(
         "num_attention_heads": 4,
         "seq_length": 256,
         "max_position_embeddings": 256,
+        "distributed_backend": "nccl",
+        "tokenizer_type": "PretrainedFromHF",
+        "tokenizer_name_or_path": "gpt2",
         }
     )
 
