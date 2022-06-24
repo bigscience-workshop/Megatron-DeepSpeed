7feec27fc1705a26f31897bdc51a9ed25e2de2b5
Lintang Sutawika <lintang@datasaur.ai>
2022-06-24 18:53:28 +0700

    enable loading multiple indexed_dataset for each field

--- tests/test_packing_dataloader.py
+++ tests/test_packing_dataloader.py
@@ -10,15 +10,30 @@ from datasets import load_dataset
 
 import torch.distributed as dist
 
-# To preprocess data before testing
+## To preprocess data before testing
+# TOKENIZER_PATH="gpt2"
+# DATA_PATH="tests/data/t0/ag_news_classify_question_first.json"
+# OUTPUT="tests/data/t0/ag_news_prompt"
 # python tools/preprocess_data.py \
-#     --input tests/data/t0/ag_news_classify_question_first.json \
-#     --output-prefix tests/data/t0/ag_news_prompt \
+#     --input $DATA_PATH \
+#     --output-prefix $OUTPUT \
 #     --dataset-impl mmap \
+#     --json-key inputs \
 #     --tokenizer-type PretrainedFromHF \
-#     --tokenizer-name-or-path gpt2 \
+#     --tokenizer-name-or-path $TOKENIZER_PATH \
 #     --append-eod \
 #     --workers 8
+
+# python tools/preprocess_data.py \
+#     --input $DATA_PATH \
+#     --output-prefix $OUTPUT \
+#     --dataset-impl mmap \
+#     --json-key targets \
+#     --tokenizer-type PretrainedFromHF \
+#     --tokenizer-name-or-path $TOKENIZER_PATH \
+#     --append-eod \
+#     --workers 8
+
 
 os.environ['MASTER_ADDR'] = 'localhost'
 os.environ['MASTER_PORT'] = '12355'
@@ -43,10 +58,13 @@ initialize_megatron(
     )
 
 train_ds, valid_ds, test_ds = build_train_valid_test_datasets(
-    data_prefix=["tests/data/t0/ag_news_prompt_text_document"],
+    data_prefix=[{
+        "input_tokens": "tests/data/t0/ag_news_prompt_inputs_document",
+        "target_tokens": "tests/data/t0/ag_news_prompt_targets_document"
+        }],
     data_impl="mmap",
     splits_string="90,5,5",
-    train_valid_test_num_samples=[100,100,100],
+    train_valid_test_num_samples=[100,0,0],
     seq_length=1024,
     seed=124,
     skip_warmup=True
