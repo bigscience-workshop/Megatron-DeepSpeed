3666319e69a1154bdd80bdf266067a7dcd37e4d3
Muennighoff <n.muennighoff@gmail.com>
2022-06-27 13:35:06 +0200

    Cleanup artefacts

--- tests/test_packing_dataloader.py
+++ tests/test_packing_dataloader.py
@@ -1,39 +1,35 @@
 import os
 import torch.distributed as dist
 
+import torch
+
 from megatron.initialize import initialize_megatron
 from megatron.data.data_samplers import MegatronPackedRandomSampler
 from megatron.data.non_causal_mtf_dataset import build_train_valid_test_datasets
 
-from datasets import load_dataset
+"""
+To preprocess data before testing
 
-## To preprocess data before testing
-# TOKENIZER_PATH="gpt2"
-# DATA_PATH="tests/data/t0/ag_news_classify_question_first.json"
-# OUTPUT="tests/data/t0/ag_news_prompt"
-# python tools/preprocess_data.py \
-#     --input $DATA_PATH \
-#     --output-prefix $OUTPUT \
-#     --dataset-impl mmap \
-#     --json-key inputs \
-#     --tokenizer-type PretrainedFromHF \
-#     --tokenizer-name-or-path $TOKENIZER_PATH \
-#     --append-eod \
-#     --workers 8
+TOKENIZER_PATH="gpt2"
+DATA_PATH="tests/data/t0/ag_news_classify_question_first.json"
+OUTPUT="tests/data/t0/ag_news_prompt"
 
-# python tools/preprocess_data.py \
-#     --input $DATA_PATH \
-#     --output-prefix $OUTPUT \
-#     --dataset-impl mmap \
-#     --json-key targets \
-#     --tokenizer-type PretrainedFromHF \
-#     --tokenizer-name-or-path $TOKENIZER_PATH \
-#     --append-eod \
-#     --workers 8
+python tools/preprocess_data.py \
+    --input $DATA_PATH \
+    --output-prefix $OUTPUT \
+    --dataset-impl mmap \
+    --json-key inputs \
+    --tokenizer-type PretrainedFromHF \
+    --tokenizer-name-or-path $TOKENIZER_PATH \
+    --append-eod \
+    --workers 8
+"""
 
-#Initialize Megatron with dummy variables
+
+# Initialize Megatron with dummy variables
 initialize_megatron(
     extra_args_provider=None,
+    allow_no_cuda=True,
     args_defaults={
         "micro_batch_size": 4,
         "num_layers": 4,
@@ -51,14 +47,14 @@ train_ds, valid_ds, test_ds = build_train_valid_test_datasets(
     data_prefix=[{
         "input_tokens": "tests/data/t0/ag_news_prompt_inputs_document",
         "target_tokens": "tests/data/t0/ag_news_prompt_targets_document"
-        }],
+    }],
     data_impl="mmap",
     splits_string="90,5,5",
     train_valid_test_num_samples=[50,0,0],
     seq_length=1024,
     seed=124,
     skip_warmup=True
-    )
+)
 
 print("Test show dataset")
 for idx in range(0,4):
@@ -67,10 +63,20 @@ for idx in range(0,4):
     print(line)
 
 
+batch_sampler = MegatronPackedRandomSampler(
+    sequence_length=256,
+    dataset=train_ds,
+    total_samples=len(256),
+    consumed_samples=0,
+    micro_batch_size=4,
+    data_parallel_rank=0,
+    data_parallel_size=1
+)
+
 dl = torch.utils.data.DataLoader(
     train_ds,
     batch_size=4,
     batch_sampler=batch_sampler,
     num_workers=4,
     pin_memory=True
-    )
+ )
