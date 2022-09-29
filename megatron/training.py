f3bcd58889a9de530f109abd7aaebd13cbbac2c4
younesbelkada <younesbelkada@gmail.com>
2022-09-29 12:11:13 +0200

    add import

--- megatron/training.py
+++ megatron/training.py
@@ -781,6 +781,8 @@ def distill_train_step(forward_step_func, data_iterator,
 
     if args.deepspeed:
         assert isinstance(student_model[0], deepspeed.PipelineEngine), student_model
+        import sys
+        sys.path.append("..")
         from ..distill_gpt import get_batch_pipe
         student_model[0].set_batch_fn(partial(get_batch_pipe, teacher_model=teacher_model))
 
