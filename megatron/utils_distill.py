94c63a1d7cc5421776f86145f50ee15fd41c20ce
younesbelkada <younesbelkada@gmail.com>
2022-09-29 12:34:36 +0200

    add print

--- megatron/utils_distill.py
+++ megatron/utils_distill.py
@@ -41,6 +41,7 @@ def get_batch_pipe_student(data, teacher_model):
         labels = labels[:, :args.curriculum_seqlen].contiguous()
         loss_mask = loss_mask[:, :args.curriculum_seqlen].contiguous()
 
+    print(tokens.shape)
     with torch.no_grad():
         teacher_logits = teacher_model[0].module(tokens)
     print(teacher_logits.shape, tokens.shape)
