36cfbd5a320c11831f01894f6a74fd04c87cf5a6
younesbelkada <younesbelkada@gmail.com>
2022-09-30 07:57:24 +0200

    discard student load

--- examples/distill_gpt.sh
+++ examples/distill_gpt.sh
@@ -185,7 +185,6 @@ export CMD=" \
     $OUTPUT_ARGS \
     --save $CHECKPOINT_PATH \
     --teacher-load $TEACHER_CHECKPOINT_PATH \
-    --student-load $CHECKPOINT_PATH \
     --train-weighted-split-paths-path $TRAIN_DATA_PATH \
     --valid-weighted-split-paths-path $VALID_DATA_PATH \
     --data-impl mmap \
