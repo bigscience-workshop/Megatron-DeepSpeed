4ecbede04b22d8e80717fc395328acc842d0e102
younesbelkada <younesbelkada@gmail.com>
2022-10-03 11:23:18 +0100

    debug

--- megatron/utils_distill.py
+++ megatron/utils_distill.py
@@ -33,10 +33,13 @@ def get_batch_pipe_student(data, teacher_model):
 
     with torch.no_grad():
         # teacher_logits = teacher_model[0].eval_batch(data_b, return_logits=True)
-        teacher_logits = teacher_model[0].module((tokens, position_ids, attention_mask))
+        teacher_logits, _ = teacher_model[0].module((tokens, position_ids, attention_mask))
+        print(teacher_model[0].module.get_tied_weights_and_groups())
+
     print(teacher_logits[0].shape)
     print(teacher_logits[1].shape)
     print(teacher_logits.shape)
+    exit()
 
     # teacher_logits = teacher_model[0].eval_batch(data_b, compute_loss = False, reduce_output = None)
     
