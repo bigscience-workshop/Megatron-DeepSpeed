f6372ac74ba0d95526eb22ed2d3ebf3015b21711
younesbelkada <younesbelkada@gmail.com>
2022-10-04 19:18:28 +0100

    try

--- megatron/training.py
+++ megatron/training.py
@@ -785,9 +785,9 @@ def distill_forward_train_step(forward_step_func, data_iterator,
 
     if args.deepspeed:
         assert isinstance(student_model[0], deepspeed.PipelineEngine), student_model[0]
-        from megatron.utils_distill import get_batch_pipe_student
+        from megatron.utils_distill import get_batch_pipe_student, get_batch_pipe_teacher
 
-        teacher_logits = teacher_model[0].eval_batch(iter(data_iterator), compute_loss = False, reduce_output = None)
+        teacher_logits = get_batch_pipe_teacher(data_iterator, teacher_model)
         print(teacher_logits.shape)
 
         student_model[0].set_batch_fn(partial(get_batch_pipe_student, teacher_logits=teacher_logits))
--- megatron/utils_distill.py
+++ megatron/utils_distill.py
@@ -3,6 +3,35 @@ from functools import partial
 from megatron import get_args, get_tokenizer, mpu
 from megatron.utils import get_ltor_masks_and_position_ids
 
+def get_batch_pipe_teacher(data, teacher_model):
+    args = get_args()
+    tokenizer = get_tokenizer()
+
+    # Items and their type.
+    keys = ['text']
+    datatype = torch.int64
+
+    # Broadcast data.
+    data_b = mpu.broadcast_data(keys, data, datatype)
+
+    # Unpack.
+    tokens_ = data_b['text'].long()
+    tokens = tokens_[:, :-1].contiguous()
+
+    # Get the masks and position ids.
+    attention_mask, _, position_ids = get_ltor_masks_and_position_ids(
+        tokens,
+        tokenizer.eod,
+        args.reset_position_ids,
+        args.reset_attention_mask,
+        args.eod_mask_loss,
+        prefix_indices=None,
+        loss_on_targets_only=args.loss_on_targets_only
+    )
+
+    teacher_logits = teacher_model[0].eval_batch(list((tokens, position_ids, attention_mask)), compute_loss = False, reduce_output = None)
+    return teacher_logits
+
 def get_batch_pipe_student(data, teacher_logits):
     """Modification of `get_batch` to work on `next(data_iterator)` instead of `data_iterator`"""
     args = get_args()
