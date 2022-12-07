c41cc5e0e2cbc4046dd9646c925f162a0d16ee41
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2022-12-07 14:04:33 +0200

    Use torch.multiprocessing.set_start_method('spawn')
    Resolves DataLoader segfaults seen in some multinode runs with PP > 1.

--- pretrain_gpt.py
+++ pretrain_gpt.py
@@ -228,6 +228,7 @@ def train_valid_test_datasets_provider(train_val_test_num_samples):
 
 @record
 def main():
+    torch.multiprocessing.set_start_method('spawn')
     pretrain(train_valid_test_datasets_provider, model_provider, forward_step,
              args_defaults={'tokenizer_type': 'GPT2BPETokenizer'})
 
