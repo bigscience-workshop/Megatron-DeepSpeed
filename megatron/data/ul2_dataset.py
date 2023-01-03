bdbd54a0de63e778bac28119b835aa381a7dad85
Muennighoff <n.muennighoff@gmail.com>
2023-01-03 23:28:41 +0400

    Fix sampling

--- megatron/data/ul2_dataset.py
+++ megatron/data/ul2_dataset.py
@@ -20,6 +20,7 @@ import numpy as np
 from megatron import get_tokenizer
 from megatron.data.dataset_utils import (
     create_masked_lm_predictions,
+    get_samples_mapping,    
     SamplingStyle
 )
 from megatron.data.t5_dataset import (
