e61e78fa2f2890409232172e855ac8baa5724397
janEbert <janpublicebert@posteo.net>
2023-02-17 15:00:49 +0100

    Refactor getting sample

--- megatron/data/gpt_dataset.py
+++ megatron/data/gpt_dataset.py
@@ -247,32 +247,37 @@ class GPTDataset(torch.utils.data.Dataset):
         return self.sample_idx.shape[0] - 1
 
     def __getitem__(self, idx):
-        # Get the shuffled index.
-        idx = self.shuffle_idx[idx]
-        # Start and end documents and offsets.
-        doc_index_f = self.sample_idx[idx][0]
-        doc_index_l = self.sample_idx[idx + 1][0]
-        offset_f = self.sample_idx[idx][1]
-        offset_l = self.sample_idx[idx + 1][1]
-        # If we are within the same document, just extract the chunk.
-        if doc_index_f == doc_index_l:
-            sample = self.indexed_dataset.get(self.doc_idx[doc_index_f],
-                                              offset=offset_f,
-                                              length=offset_l - offset_f + 1)
-        else:
-            # Otherwise, get the rest of the initial document.
-            sample_list = [self.indexed_dataset.get(self.doc_idx[doc_index_f],
-                                                    offset=offset_f)]
-            # Loop over all in between documents and add the entire document.
-            for i in range(doc_index_f + 1, doc_index_l):
-                sample_list.append(self.indexed_dataset.get(self.doc_idx[i]))
-            # And finally add the relevant portion of last document.
-            sample_list.append(self.indexed_dataset.get(
-                self.doc_idx[doc_index_l],
-                length=offset_l + 1))
-            sample = np.concatenate(sample_list)
-
+        sample = get_sample(self.indexed_dataset, self.doc_idx,
+                            self.sample_idx, self.shuffle_idx, idx)
         return {'text': np.array(sample, dtype=np.int64)}
+
+
+def get_sample(indexed_dataset, doc_idx, sample_idx, shuffle_idx, idx):
+    # Get the shuffled index.
+    idx = shuffle_idx[idx]
+    # Start and end documents and offsets.
+    doc_index_f = sample_idx[idx][0]
+    doc_index_l = sample_idx[idx + 1][0]
+    offset_f = sample_idx[idx][1]
+    offset_l = sample_idx[idx + 1][1]
+    # If we are within the same document, just extract the chunk.
+    if doc_index_f == doc_index_l:
+        sample = indexed_dataset.get(doc_idx[doc_index_f],
+                                     offset=offset_f,
+                                     length=offset_l - offset_f + 1)
+    else:
+        # Otherwise, get the rest of the initial document.
+        sample_list = [indexed_dataset.get(doc_idx[doc_index_f],
+                                           offset=offset_f)]
+        # Loop over all in between documents and add the entire document.
+        for i in range(doc_index_f + 1, doc_index_l):
+            sample_list.append(indexed_dataset.get(doc_idx[i]))
+        # And finally add the relevant portion of last document.
+        sample_list.append(indexed_dataset.get(
+            doc_idx[doc_index_l],
+            length=offset_l + 1))
+        sample = np.concatenate(sample_list)
+    return sample
 
 
 def build_index_mappings(name, data_prefix, documents, sizes,
